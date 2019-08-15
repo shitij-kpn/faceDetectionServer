336348e41f746afdeab2cd7df59de81febde926e
shitij <shitijtalan@gmail.com>
2019-08-15 20:14:00 +0530

    database

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const knex = require('knex');
 const postgres = knex({
     client:'pg',
     connection: {
-        host: process.env.DATABASE_URL,
+        connectionString: process.env.DATABASE_URL,
         ssl:true
     }
 });
